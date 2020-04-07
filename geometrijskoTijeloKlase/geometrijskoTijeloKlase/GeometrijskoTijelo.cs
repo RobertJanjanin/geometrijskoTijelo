117b604d27f262d483633e1a78ff90b6562969a9
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-04-07 11:54:30 +0200

    Add files via upload

---
+++ geometrijskoTijeloKlase/geometrijskoTijeloKlase/GeometrijskoTijelo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometrijskoTijeloKlase
+{
+    class GeometrijskoTijelo
+    {
+     
+        static int brojacTijela;
+         double volumen;
+        double oplosje;
+        public static int BrojacTijela { get => brojacTijela; set => brojacTijela = value; }
+
+
+        public double Volumen { get => volumen; set => volumen = value; }
+
+        public double Oplosje { get => oplosje; set => oplosje = value; }
+
+       
+        
+        public GeometrijskoTijelo()
+        {
+
+          BrojacTijela++;
+            
+        }
+       
+    }
+}
